36a169e507986d99b9b895398a2d635c509ddfa2
Tony Huang <yellow24681379@gmail.com>
2017-05-15 10:17:44 +0800

    add import.sh

---
+++ iNu-grafana/import.sh
@@ -0,0 +1,5 @@
+kubectl create configmap grafana-import-dashboards --from-file import/ -n kube-system --dry-run -o yaml >templates/grafana-import-dashboards-configMap.yml
+
+namespace=$(grep namespace: values.yaml)
+printf "  ${namespace}" >> templates/grafana-import-dashboards-configMap.yml
+sed -i 's/{{\(.*\)}}/{{"{{"}}\1{{"}}"}}/g' templates/grafana-import-dashboards-configMap.yml
