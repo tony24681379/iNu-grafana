adf4a1991af7e97c5171ce51cb183d3411957ca7
Tony Huang <yellow24681379@gmail.com>
2017-05-15 11:31:25 +0800

    fix sed command

--- iNu-grafana/import.sh
+++ iNu-grafana/import.sh
@@ -2,4 +2,4 @@ kubectl create configmap grafana-import-dashboards --from-file import/ -n kube-s
 
 namespace=$(grep namespace: values.yaml)
 printf "  ${namespace}" >> templates/grafana-import-dashboards-configMap.yml
-sed -i 's/{{\(.*\)}}/{{"{{"}}\1{{"}}"}}/g' templates/grafana-import-dashboards-configMap.yml
+sed -i 's/{{\([^{}]*\)}}/{{"{{"}}\1{{"}}"}}/g' templates/grafana-import-dashboards-configMap.yml
