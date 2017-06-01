c865ec0e3bb96470c42b98ea3a7fd3828348feb3
Tony Huang <yellow24681379@gmail.com>
2017-06-01 11:05:27 +0800

    add mac support

--- iNu-grafana/import.sh
+++ iNu-grafana/import.sh
@@ -2,4 +2,8 @@ kubectl create configmap grafana-import-dashboards --from-file import/ -n kube-s
 
 namespace=$(grep namespace: values.yaml)
 printf "  ${namespace}" >> templates/grafana-import-dashboards-configMap.yml
-sed -i 's/{{\([^{}]*\)}}/{{"{{"}}\1{{"}}"}}/g' templates/grafana-import-dashboards-configMap.yml
+if [ "$(uname)" == "Darwin" ]; then
+    sed -i '.bak' 's/{{\([^{}]*\)}}/{{"{{"}}\1{{"}}"}}/g' templates/grafana-import-dashboards-configMap.yml      
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sed -i 's/{{\([^{}]*\)}}/{{"{{"}}\1{{"}}"}}/g' templates/grafana-import-dashboards-configMap.yml
+fi
