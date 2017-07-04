8f45f81f2814599ec980b683a38c28c6ffab2dd0
Tony Huang <yellow24681379@gmail.com>
2017-07-04 09:58:13 +0800

    remove import.sh

--- iNu-grafana/import.sh
+++
@@ -1,9 +0,0 @@
-kubectl create configmap grafana-import-dashboards --from-file import/ -n kube-system --dry-run -o yaml >templates/grafana-import-dashboards-configMap.yml
-
-namespace=$(grep namespace: values.yaml)
-printf "  ${namespace}" >> templates/grafana-import-dashboards-configMap.yml
-if [ "$(uname)" == "Darwin" ]; then
-    sed -i '.bak' 's/{{\([^{}]*\)}}/{{"{{"}}\1{{"}}"}}/g' templates/grafana-import-dashboards-configMap.yml      
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    sed -i 's/{{\([^{}]*\)}}/{{"{{"}}\1{{"}}"}}/g' templates/grafana-import-dashboards-configMap.yml
-fi
